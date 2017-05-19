19855aeaaf03075d035f9bfea1c15fdb14f44046
Amy Mok <amy.mok@cfpb.gov>
2017-05-19 13:28:08 -0400

    Fix import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 import os
 
-here = os.path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
