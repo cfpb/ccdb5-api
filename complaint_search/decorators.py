4302ccb49274e29b897c91601764149bb24ec8d6
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2018-06-22 09:34:00 -0400

    feat: Log the error details

--- complaint_search/decorators.py
+++ complaint_search/decorators.py
@@ -1,13 +1,18 @@
+import logging
 from rest_framework import status
 from rest_framework.response import Response
 from elasticsearch import TransportError
+
+log = logging.getLogger(__name__)
 
 
 def catch_es_error(function):
     def wrap(request, *args, **kwargs):
         try:
             return function(request, *args, **kwargs)
-        except TransportError as e:
+        except TransportError as te:
+            log.error(te)
+
             status_code = 424  # HTTP_424_FAILED_DEPENDENCY
             res = {
                 "error": 'There was an error calling Elasticsearch'
