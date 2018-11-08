171d03af9ec48e2e43ffb9fa2e1a1a2b18fb9704
Adam Zarger <adam.zarger@cfpb.gov>
2018-11-08 10:14:48 -0500

    Add export_csv script and update es_interface to call script

--- complaint_search/es_interface.py
+++ complaint_search/es_interface.py
@@ -7,7 +7,7 @@ from datetime import datetime, date, timedelta
 from collections import defaultdict, namedtuple
 import requests
 import logging
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 from flags.state import flag_enabled
 from complaint_search.es_builders import (
     SearchBuilder,
@@ -24,6 +24,8 @@ from stream_content import (
     StreamCSVContent,
     StreamJSONContent,
 )
+
+from export_csv import ElasticSearchExporter
 
 _ES_URL = "{}://{}:{}".format("http", os.environ.get('ES_HOST', 'localhost'),
                               os.environ.get('ES_PORT', '9200'))
@@ -187,37 +189,48 @@ def search(agg_exclude=None, **kwargs):
         res["_meta"] = _get_meta()
 
     elif format in EXPORT_FORMATS:
-        # Deleting from field and this will force data format plugin to use
-        # scan/scroll query to create the content,
-        # Size also doesn't seem to be relevant anymore
-        del(body["from"])
+        # # Deleting from field and this will force data format plugin to use
+        # # scan/scroll query to create the content,
+        # # Size also doesn't seem to be relevant anymore
+        # del(body["from"])
 
-        p = {
-            "format": format,
-            "source": json.dumps(body),
-            "fl": ",".join(field for field in CSV_ORDERED_HEADERS.keys()),
-            "append.header": "false"
-        }
-        p = urllib.urlencode(p)
+        # p = {
+        #     "format": format,
+        #     "source": json.dumps(body),
+        #     "fl": ",".join(field for field in CSV_ORDERED_HEADERS.keys()),
+        #     "append.header": "false"
+        # }
+        # p = urllib.urlencode(p)
 
-        url = "{}/{}/{}/_data?{}".format(_ES_URL, _COMPLAINT_ES_INDEX,
-                                         _COMPLAINT_DOC_TYPE, p)
+        # url = "{}/{}/{}/_data?{}".format(_ES_URL, _COMPLAINT_ES_INDEX,
+        #                                  _COMPLAINT_DOC_TYPE, p)
 
-        # requests.get does not seem to respect an IP address in NO_PROXY.
-        # This is a workaround based on:
-        # https://stackoverflow.com/questions/28521535/requests-how-to-disable-bypass-proxy/28521696#28521696  # noqa
-        session = requests.Session()
-        session.trust_env = False
-        response = session.get(url, auth=(
-            _ES_USER, _ES_PASSWORD), stream=True)
-        if response.ok:
-            res = response.iter_content(chunk_size=CHUNK_SIZE)
-            if format == "json":
-                res = StreamJSONContent(res)
-            elif format == "csv":
-                readable_header = ",".join('"' + rfield + '"'
-                                           for rfield in CSV_ORDERED_HEADERS.values()) + "\n"
-                res = StreamCSVContent(readable_header, res)
+        # # requests.get does not seem to respect an IP address in NO_PROXY.
+        # # This is a workaround based on:
+        # # https://stackoverflow.com/questions/28521535/requests-how-to-disable-bypass-proxy/28521696#28521696  # noqa
+        # session = requests.Session()
+        # session.trust_env = False
+        # response = session.get(url, auth=(
+        #     _ES_USER, _ES_PASSWORD), stream=True)
+        # if response.ok:
+        #     res = response.iter_content(chunk_size=CHUNK_SIZE)
+        #     if format == "json":
+        #         res = StreamJSONContent(res)
+        #     elif format == "csv":
+        #         readable_header = ",".join('"' + rfield + '"'
+        #                                    for rfield in CSV_ORDERED_HEADERS.values()) + "\n"
+        #         res = StreamCSVContent(readable_header, res)
+
+        # Test for export_2
+        scanResponse = helpers.scan(client=_get_es(), query=body, scroll= "10m", 
+                index=params.get("index_name"), size=7000, doc_type=_COMPLAINT_DOC_TYPE, 
+                request_timeout=3000)
+
+        exporter = ElasticSearchExporter()
+        res = exporter.export_2(
+                scanResponse,
+                CSV_ORDERED_HEADERS)
+
     return res
 
 
