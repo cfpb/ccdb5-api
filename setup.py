406a90f2d20a1102cc693698b083a3ae151421c0
Will Barton <william.barton@cfpb.gov>
2019-06-21 14:18:45 -0400

    Add Python 3.6 classifier

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django :: 1.11',
     ],
     keywords='complaint search api',
