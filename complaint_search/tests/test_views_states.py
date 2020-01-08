c4bd9f958f5d87903aa359a74ce072e9a206c0ff
Adam Zarger <adam.zarger@cfpb.gov>
2020-01-08 17:18:41 -0500

    Unit tests

---
+++ complaint_search/tests/test_views_states.py
@@ -0,0 +1,71 @@
+import copy
+from django.core.urlresolvers import reverse
+
+import mock
+from complaint_search.serializer import SearchInputSerializer
+from complaint_search.defaults import (
+    AGG_EXCLUDE_FIELDS,
+    PARAMS,
+)
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class StatesTests(APITestCase):
+
+    def setUp(self):
+        pass
+
+    def buildDefaultParams(self, overrides):
+        params = copy.deepcopy(PARAMS)
+        params.update(overrides)
+        return params
+
+    @mock.patch('complaint_search.es_interface.states_agg')
+    def test_states_no_param(self, mock_essearch):
+        """
+        Searching with no parameters
+        """
+        url = reverse('complaint_search:states')
+        mock_essearch.return_value = 'OK'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        mock_essearch.assert_called_once_with(agg_exclude=AGG_EXCLUDE_FIELDS,
+                                              **self.buildDefaultParams({}))
+        self.assertEqual('OK', response.data)
+
+    @mock.patch('complaint_search.es_interface.states_agg')
+    def test_states_with_field__valid(self, mock_essearch):
+        url = reverse('complaint_search:states')
+        for field in SearchInputSerializer.FIELD_CHOICES:
+            params = {"field": field[0]}
+            mock_essearch.return_value = 'OK'
+            response = self.client.get(url, params)
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+            self.assertEqual('OK', response.data)
+
+        calls = [
+            mock.call(
+                agg_exclude=AGG_EXCLUDE_FIELDS,
+                **self.buildDefaultParams({
+                    "field": SearchInputSerializer.FIELD_MAP.get(
+                        field_pair[0],
+                        field_pair[0]
+                    )})
+            )
+            for field_pair in SearchInputSerializer.FIELD_CHOICES
+        ]
+        mock_essearch.assert_has_calls(calls)
+
+    @mock.patch('complaint_search.es_interface.states_agg')
+    def test_state_with_field__invalid(self, mock_essearch):
+        url = reverse('complaint_search:states')
+        params = {"field": "invalid_choice"}
+        mock_essearch.return_value = 'OK'
+        response = self.client.get(url, params)
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        mock_essearch.assert_not_called()
+        self.assertDictEqual(
+            {"field": ["\"invalid_choice\" is not a valid choice."]},
+            response.data
+        )
