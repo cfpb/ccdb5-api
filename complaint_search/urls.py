0cf5b32d6f548902744dcebb180113545e7a80b4
Adam Zarger <adam.zarger@cfpb.gov>
2020-01-15 08:28:43 -0500

    Refactor urls.py and redirect /geo

--- complaint_search/urls.py
+++ complaint_search/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 import complaint_search.views
 
@@ -15,10 +16,12 @@ urlpatterns = [
         name="suggest_zip"
     ),
     url(r'^_suggest', complaint_search.views.suggest, name="suggest"),
-    url(r'^states/$', complaint_search.views.states,
-        name="states"),
     url(
         r'^(?P<id>[0-9]+)$', complaint_search.views.document, name="complaint"
     ),
     url(r'^$', complaint_search.views.search, name="search"),
+    url(r'^geo/states', complaint_search.views.states,
+        name="states"),
+    url(r'^geo',
+        RedirectView.as_view(url='/geo/states'), name="geo"),
 ]
