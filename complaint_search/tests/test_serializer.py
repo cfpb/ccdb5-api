f75aa7dde965363fb281d6cc02ecabbbb9194fb7
Adam Zarger <adam.zarger@cfpb.gov>
2020-06-10 13:48:11 -0400

    Tests for trends serializer

--- complaint_search/tests/test_serializer.py
+++ complaint_search/tests/test_serializer.py
@@ -4,7 +4,10 @@ import copy
 from django.test import TestCase
 
 from complaint_search.defaults import PARAMS
-from complaint_search.serializer import SearchInputSerializer
+from complaint_search.serializer import (
+    SearchInputSerializer,
+    TrendsInputSerializer,
+)
 
 
 class SearchInputSerializerTests(TestCase):
@@ -60,3 +63,88 @@ class SearchInputSerializerTests(TestCase):
             u'Issue is malformed, it needs to be "issue" or '
             '"issue\u2022subissue"'
         ])
+
+
+class TrendsInputSerializerTests(TestCase):
+
+    def setUp(self):
+        self.data = {
+        }
+
+    def test_is_valid___no_required_params(self):
+        serializer = TrendsInputSerializer(data={})
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['trend_interval'], [
+            'This field is required.'
+        ])
+
+    def test_is_valid__default_requred_params(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'month',
+            'lens': 'overview'
+        })
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_is_valid__invalid_lens(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'foo'
+        })
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['lens'], [
+            '"foo" is not a valid choice.'
+        ])
+
+    def test_is_valid__valid_lens(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'overview'
+        })
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_is_valid__no_sub_lens(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'product'
+        })
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['non_field_errors'], [
+            "Either Focus or Sub-lens is required for lens 'product'."
+            " Valid sub-lens are: ('sub_product', 'issue', 'company', 'tags')"
+        ])
+
+    def test_is_valid__invalid_sub_lens(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'product',
+            'sub_lens': 'foobar'
+        })
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['non_field_errors'], [
+            "'foobar' is not a valid sub-lens for 'product'. Valid "
+            "sub-lens are: ('sub_product', 'issue', 'company', 'tags')"
+        ])
+
+    def test_is_valid__valid_sub_lens(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'product',
+            'sub_lens': 'company'
+        })
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_is_valid__valid_focus(self):
+        serializer = TrendsInputSerializer(data={
+            'trend_interval': 'year',
+            'lens': 'product',
+            'focus': 'Debt collection'
+        })
+
+        self.assertTrue(serializer.is_valid())
