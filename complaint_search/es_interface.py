0218609f3fecee47a5238f96c0a34e685dafe999
higs4281 <higs4281@gmail.com>
2021-08-06 11:59:16 -0400

    Remove unneeded hits from map-tab requests
    A test `size` value was errantly left in the es_interface.states_agg function.
    The map tab does not need any hits, so size should be 0 for efficiency.

--- complaint_search/es_interface.py
+++ complaint_search/es_interface.py
@@ -440,7 +440,7 @@ def document(complaint_id):
 def states_agg(agg_exclude=None, **kwargs):
     params = copy.deepcopy(PARAMS)
     params.update(**kwargs)
-    params.update({'size': 500})
+    params.update({'size': 0})
     search_builder = SearchBuilder()
     search_builder.add(**params)
     body = search_builder.build()
