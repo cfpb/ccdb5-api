1c2575a477fdab9082f4c785b6c62f7918632a2d
higginsw <william.higgins@cfpb.gov>
2023-02-10 15:56:32 -0500

    Deployed 4929c8f with MkDocs version: 0.17.5

--- documentation/swagger-initializer.js
+++ documentation/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://raw.githubusercontent.com/cfpb/ccdb5-api/main/swagger-config.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
