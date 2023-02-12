9e0c6ba2a46ef5944294e92e10925841a0248fab
higginsw <william.higgins@cfpb.gov>
2023-02-11 23:54:23 -0500

    Deployed 534107d with MkDocs version: 0.17.5

--- documentation/swagger-initializer.js
+++ documentation/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "https://raw.githubusercontent.com/cfpb/ccdb5-api/main/swagger-config.yaml",
-    url: "https://raw.githubusercontent.com/cfpb/ccdb5-api/fixdocs/swagger-config.yaml"
+    url: "https://raw.githubusercontent.com/cfpb/ccdb5-api/fixdocs/swagger-config.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
